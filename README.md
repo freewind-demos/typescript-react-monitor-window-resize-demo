27037b378475a1163b9ea49b7fa9ea8aee3312a5
Freewind <nowindlee@gmail.com>
2020-07-08 15:27:06 +0800

    typescript-react-monitor-window-resize-demo

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-TypeScript React Hello World Demo
-=================================
+TypeScript React Monitor Window Resize Demo
+==========================================
+
+Write a react hook to monitor window resizing
+
+https://stackoverflow.com/a/19014495/342235
 
 ```
 npm install
--- src/hello.tsx
+++ src/hello.tsx
@@ -1,8 +1,10 @@
-//import * as React from 'react'
 import React from 'react'
+import useWindowSize from "./useWindowSize";
 
 export default function Hello() {
+  const [width, height] = useWindowSize();
   return <div>
-    <h1>Hello React</h1>
+    <h1>Resize the window</h1>
+    <div>Current window size: {width} x {height}</div>
   </div>
 };
